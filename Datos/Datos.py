72f97d513267eeb12ae16dea7a81c3112c5767ca
juampifranco23 <juampifranco23@gmail.com>
2026-09-16 21:51:35 -0300

    filtrado por fechas (tambien se puede usar para busqueda de 1 fecha en específico) y busqueda por el nombre de una categoria

--- Datos/Datos.py
+++ Datos/Datos.py
@@ -5,10 +5,11 @@ clave_login=(nombre,contrasena)
 
 
 
-'''Datos'''
-
+'''Datos
 # Categorías
+#testing
 Id_Categoria = ["1", "2", "3", "4", "5"]
+
 NombreC = ["Alimentos", "Transporte", "Ocio", "Salud", "Educación"]
 DescripcionC = [
     "Categoria de alimentos",
@@ -19,7 +20,17 @@ DescripcionC = [
 ]
 EstadoC = ["Activo", "Activo", "Activo", "Activo", "Activo"]
 categoria = [Id_Categoria, NombreC, DescripcionC, EstadoC]
-encabezadosC = ["Id_Categoria", "Nombre", "Descripcion"]
+encabezadosC = ["Id_Categoria", "Nombre", "Descripcion", "Estado"]
+'''
+Id_Categoria = ["1", "2", "3"]
+NombreC = ["Alimentos", "Transporte", "Ocio"]
+DescripcionC = [
+    "Categoria de alimentos",
+    "Categoria de transporte",
+    "Categoria de ocio"]
+EstadoC = ["Activo", "Activo", "Activo"]
+categoria = [Id_Categoria, NombreC, DescripcionC, EstadoC]
+encabezadosC = ["ID", "Nombre", "Descripcion", "Estado"]
 
 # Presupuestos
 Id_Presupuesto = ["1", "2", "3", "4", "5"]
@@ -31,29 +42,60 @@ Periodo_Presupuesto = [
     "05/08/2026-15/08/2026",
 ]
 Monto_limite = [120.0, 45.0, 25.0, 58.0, 80.0]
-Id_CategoriaP = ["1", "2", "3", "4", "5"]
+
+Cat_fk = ["2", "3", "1", "2", "1"]
 EstadoP = ["Activo", "Activo", "Activo", "Activo", "Activo"]
-presupuestos = [Id_Presupuesto, Periodo_Presupuesto, Monto_limite, Id_CategoriaP, EstadoP]
-encabezadosP = ["Id_Presupuesto", "Periodo", "Monto Limite", "Id_Categoria"]
+presupuestos = [Id_Presupuesto, Cat_fk, Monto_limite, Periodo_Presupuesto, EstadoP]
+encabezadosP = ["Id_Presupuesto", "ID-Cat-Nombre", "Periodo", "Monto Limite", "Estado"]
 
 # Gastos
-NombreG = ["Supermercado", "Gasolina", "Cine", "Farmacia", "Curso online"]
-Id_Gasto = ["1", "2", "3", "4", "5"]
-FechaG = ["01/08/2026", "02/08/2026", "03/08/2026", "04/08/2026", "05/08/2026"]
-MontoG = [100.0, 40.0, 20.0, 50.0, 70.0]
-DescripcionG = [
-    "Compra de alimentos",
-    "Llenado de tanque",
-    "Entrada de cine",
-    "Compra de medicamentos",
-    "Pago de curso online",
+encabezadosG = ["Id_Gasto", "Nombre", "Fecha", "Monto", "Descripcion", "Estado"]
+
+gastos = [
+    {
+        "id_gasto": "1",
+        "nombre": "Supermercado",
+        "fecha": "01/08/2026",
+        "monto": 100.0,
+        "descripcion": "Compra de alimentos",
+        "estado": True
+    },
+    {
+        "id_gasto": "2",
+        "nombre": "Gasolina",
+        "fecha": "02/08/2026",
+        "monto": 40.0,
+        "descripcion": "Llenado de tanque",
+        "estado": True
+    },
+    {
+        "id_gasto": "3",
+        "nombre": "Cine",
+        "fecha": "03/08/2026",
+        "monto": 20.0,
+        "descripcion": "Entrada de cine",
+        "estado": True
+    },
+    {
+        "id_gasto": "4",
+        "nombre": "Farmacia",
+        "fecha": "04/08/2026",
+        "monto": 50.0,
+        "descripcion": "Compra de medicamentos",
+        "estado": True
+    },
+    {
+        "id_gasto": "5",
+        "nombre": "Curso online",
+        "fecha": "05/08/2026",
+        "monto": 70.0,
+        "descripcion": "Pago de curso online",
+        "estado": True
+    }
 ]
-# ??? pq  creamos 90 listas de IDS si se suponen los ID vienen de las otras tablas
-Id_CatGasto = ["1", "2", "3", "4", "5"]
-Id_PresGasto = ["1", "2", "3", "4", "5"]
-EstadoG = ["Activo", "Activo", "Activo", "Activo", "Activo"]
-gastos = [ NombreG, FechaG, MontoG, DescripcionG, Periodo_Presupuesto]
-encabezadosG = ["Id_Gasto", "Nombre", "Fecha", "Monto", "Descripcion", "Id_Categoria", "Id_Presupuesto"]
 
 '''datos de porcentaje'''
-datos_porcentaje=[NombreG,MontoG]
+datos_porcentaje = [
+    [g["nombre"] for g in gastos], 
+    [g["monto"] for g in gastos]
+]
